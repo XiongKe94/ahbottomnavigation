eed6770304f0f7c2218dff7535d282156bc021a1
熊科 <xiongke@apkpure.net>
2020-08-21 18:00:52 +0800

    no message

--- ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
@@ -250,7 +250,6 @@ public class AHBottomNavigation extends FrameLayout {
         notificationAnimationDuration = 150;
 
         ViewCompat.setElevation(this, resources.getDimension(R.dimen.bottom_navigation_elevation));
-        setClipToPadding(false);
 
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, bottomNavigationHeight);
@@ -486,9 +485,12 @@ public class AHBottomNavigation extends FrameLayout {
                 icon.setImageDrawable(getCurrentDrawable(i, current));
                 title.setTextColor(current ? itemActiveColor : itemInactiveColor);
                 view.setSoundEffectsEnabled(soundEffectsEnabled);
+                view.setEnabled(true);
             } else {
                 icon.setImageDrawable(getEnabledDrawable(i));
                 title.setTextColor(itemDisableColor);
+                view.setClickable(true);
+                view.setEnabled(false);
             }
 
             LayoutParams params = new LayoutParams((int) itemWidth, (int) height);
@@ -626,10 +628,13 @@ public class AHBottomNavigation extends FrameLayout {
                     }
                 });
                 view.setSoundEffectsEnabled(soundEffectsEnabled);
+                view.setEnabled(true);
             } else {
                 icon.setImageDrawable(getEnabledDrawable(i));
                 title.setTextColor(itemDisableColor);
                 title.setAlpha(0);
+                view.setClickable(true);
+                view.setEnabled(false);
             }
 
             int width = i == currentItem ? (int) selectedItemWidth :
